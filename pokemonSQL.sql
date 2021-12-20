74bc6f69bdbaac1250b31a70546c0ba2e677c883
[martinezk23] <[mkaren2301@gail.com]>
2021-12-20 11:38:35 -0600

    first commit

---
+++ pokemonSQL.sql
@@ -0,0 +1,45 @@
+--Pokemon Challange
+
+--DROP TABLE Pokemon
+--DROP TABLE Type
+--DROP TABLE PokemonType
+
+CREATE TABLE Pokemon
+(
+	Id INT (500) PRIMARY KEY NOT NULL, --need to increment??
+	PokemonName VARCHAR (15) NOT NULL,
+	PokemonHeight VARCHAR (10) NOT NULL,
+	PokemonWeight VARCHAR (10) NOT NULL
+); 
+
+CREATE TABLE Type
+(
+	Id INT (500), --PRIMARY KEY NOT NULL, 
+	typeId INT (500) NOT NULL,
+	Name NVARCHAR (100) NOT NULL,
+);
+
+CREATE TABLE PokemonType
+(
+	id INT (500) IDENTITY PRIMARY KEY,
+	PokemonId INT (500) FOREIGN KEY REFERENCES Pokemon (ID) NOT NULL,
+	TypeId INT (500) FOREIGN KEY REFERENCES Type (typeId) NOT NULL,
+);
+
+
+--Inserting data to tables
+INSERT Pokemon
+	(id, PokemonName, PokemonHeight, PokemonWeight)
+VALUES
+	(1, Bulbasur, 7, 70),
+	(2, Dittom, 3, 40);
+
+INSERT Type
+	(typeId, Name)
+VALUES
+	(2, Normal),
+	(1, Grass);
+
+
+
+SELECT * FROM Pokemon
