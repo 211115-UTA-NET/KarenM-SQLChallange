ccb4202d5099852bb1f9d2cd086ec8e820c1c39a
[martinezk23] <[mkaren2301@gail.com]>
2021-12-20 11:58:08 -0600

    Tables created

--- pokemonSQL.sql
+++ pokemonSQL.sql
@@ -6,24 +6,25 @@
 
 CREATE TABLE Pokemon
 (
-	Id INT (500) PRIMARY KEY NOT NULL, --need to increment??
+	--Id INT IDENTITY PRIMARY KEY NOT NULL, did not work 
+	Id INT NOT NULL IDENTITY PRIMARY KEY , 
 	PokemonName VARCHAR (15) NOT NULL,
 	PokemonHeight VARCHAR (10) NOT NULL,
 	PokemonWeight VARCHAR (10) NOT NULL
 ); 
 
-CREATE TABLE Type
+CREATE TABLE [Type] 
 (
-	Id INT (500), --PRIMARY KEY NOT NULL, 
-	typeId INT (500) NOT NULL,
-	Name NVARCHAR (100) NOT NULL,
+	Id INT NOT NULL IDENTITY PRIMARY KEY , 
+	typeId INT NOT NULL (500),
+	[Name] NVARCHAR NOT NULL (100), 
 );
 
-CREATE TABLE PokemonType
+CREATE TABLE PokemonType --pokemonid and typeide need to reference back to pokemon and type.
 (
-	id INT (500) IDENTITY PRIMARY KEY,
+	Id INT NOT NULL IDENTITY PRIMARY KEY , 
 	PokemonId INT (500) FOREIGN KEY REFERENCES Pokemon (ID) NOT NULL,
-	TypeId INT (500) FOREIGN KEY REFERENCES Type (typeId) NOT NULL,
+	TypeId INT (500) FOREIGN KEY REFERENCES [Type] (typeId) NOT NULL,
 );
 
 
@@ -34,8 +35,8 @@ VALUES
 	(1, Bulbasur, 7, 70),
 	(2, Dittom, 3, 40);
 
-INSERT Type
-	(typeId, Name)
+INSERT [Type]
+	(typeId, [Name])
 VALUES
 	(2, Normal),
 	(1, Grass);
